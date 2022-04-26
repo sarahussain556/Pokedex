8651f95276deaf49303c4f10c9b7703e4681438c
Sara Hussain <85155050+sarahussain556@users.noreply.github.com>
2022-04-26 16:55:43 +0530

    Adding detail component of pokemon

--- src/app/pokemon-display/pokemon-display.component.ts
+++ src/app/pokemon-display/pokemon-display.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Location } from '@angular/common';
+
+type NavigationObject={
+  navigationId:number;
+}
+type Ability={
+  name:string;
+}
 @Component({
   selector: 'app-pokemon-display',
   templateUrl: './pokemon-display.component.html',
@@ -7,9 +16,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonDisplayComponent implements OnInit {
 
-  constructor() { }
+  // constructor(
+  //  // private dataService:DataService
+  // ) { }
+  constructor(private location:Location,private dataService:DataService,public abilities:Array<string>){
+
+  }
+
 
   ngOnInit(): void {
+    
+    let navigationObject=this.location.getState() as NavigationObject;
+      console.log(navigationObject);
+      this.dataService.getPokemonInfo(navigationObject.navigationId)
+      .subscribe((response:any)=>{
+        console.log(response);
+        this.abilities=response.abilities.map((ability:Ability)=>{ return ability.name});
+        
+     // this.pokemons=[...response.results]
+      //console.log(this.pokemons);
+      })
   }
 
 }
